23f389858ce7d8ba39c6c2621119ab0273e4db1b
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-08-20 12:08:33 +0800

    Level-3: Add mark as done functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
         
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         String input;
         
@@ -21,12 +21,27 @@ public class Duke {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int taskNum = Integer.parseInt(input.substring(5)) - 1;
+                tasks[taskNum].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskNum]);
+                System.out.println("____________________________________________________________");
+            } else if (input.startsWith("unmark ")) {
+                int taskNum = Integer.parseInt(input.substring(7)) - 1;
+                tasks[taskNum].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskNum]);
+                System.out.println("____________________________________________________________");
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
