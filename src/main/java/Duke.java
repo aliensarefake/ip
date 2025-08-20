0ec0c7af0c560df7a9bd6b5ac2561617ba3d6ca6
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-08-20 12:07:35 +0800

    Level-2: Add support for adding and listing tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,6 +8,8 @@ public class Duke {
         System.out.println("____________________________________________________________");
         
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String input;
         
         while (true) {
@@ -17,10 +19,19 @@ public class Duke {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
         
         scanner.close();
