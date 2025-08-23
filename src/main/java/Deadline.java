f7fbf3e568ef5da9a37178b5974db51e217c7e27
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-08-23 20:46:57 +0800

    Level-8: Add date and time parsing with LocalDate and LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,27 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     protected String by;
+    protected LocalDate date;
 
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+        try {
+            this.date = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            this.date = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        String dateStr = by;
+        if (date != null) {
+            dateStr = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        }
+        return "[D]" + super.toString() + " (by: " + dateStr + ")";
     }
 }
