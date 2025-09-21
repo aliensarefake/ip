a9f8d0fe38977bed5e0fcb83beb2f07ee137ca7d
aliensarefake <guoxiaoyu2003429@gmail.com>
2025-09-21 17:44:24 +0800

    Fix rendering issue across OS

--- src/main/java/taskbot/Launcher.java
+++ src/main/java/taskbot/Launcher.java
@@ -9,6 +9,9 @@ public final class Launcher {
     private Launcher() {
     }
     public static void main(String[] args) {
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("javafx.animation.pulse", "60");
         Application.launch(Main.class, args);
     }
 }
